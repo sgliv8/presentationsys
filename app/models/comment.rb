206cb383d5f036700946edcb72c8db53d3dc868f
Jay Xu <sgliv8@gmail.com>
2015-07-28 01:42:22 -0700

    change the valiation of the models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,8 +4,4 @@ class Comment < ActiveRecord::Base
 
   validates_presence_of :group, :message => "Group number is required."
   validates_presence_of :project, :message => "Project name is required."
-  validates_presence_of :strength, :message => "Question 1 is required."
-  validates_presence_of :weakness, :message => "Question 2 is required." 
-  validates_presence_of :improvement, :message => "Question 3 is required."
-  validates_presence_of :fund, :message => "Question 4 is required."
 end
--- app/models/grade.rb
+++ app/models/grade.rb
@@ -5,13 +5,4 @@ class Grade < ActiveRecord::Base
 
   validates_presence_of :group, :message => "Group number is required."
   validates_presence_of :project, :message => "Project name is required."
-  validates_presence_of :opport, :message => "Question a is required."
-  validates_presence_of :cost, :message => "Question b is required." 
-  validates_presence_of :timeline, :message => "Question c is required." 
-  validates_presence_of :obstacle, :message => "Question d is required." 
-  validates_presence_of :risk, :message => "Question e is required." 
-  validates_presence_of :point, :message => "Question f is required." 
-  validates_presence_of :fund, :message => "Question g is required." 
-  validates_presence_of :totalscore, :message => "Final grade is required." 
-  # validates_presence_of :course_id, :message => "Course number is required."
 end
